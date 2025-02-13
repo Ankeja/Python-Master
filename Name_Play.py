7cd4331469cad250ed11e60ce7c96d346400078c
“[Andrew <andrew.jacobs9000@gmail.com>
2025-02-13 10:27:04 +0000

    printing my name with variables and concantenation

---
+++ Name_Play.py
@@ -0,0 +1,9 @@
+#printing my name using a variable
+#msg = "Andrew Jacobs"
+#print(msg)
+
+#printing my name via concatenation
+first_name = "Andrew"
+last_name = "Jacobs"
+full_name = first_name +" "+ last_name
+print(full_name)
