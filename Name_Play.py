e608caae4f49433d845fd9f3b8c1da11e81b7bcc
Ankeja <Andrew.Jacobs9000@gmail.com>
2025-04-26 22:19:00 +0100

    tidying

--- Name_Play.py
+++
@@ -1,9 +0,0 @@
-#printing my name using a variable
-#msg = "Andrew Jacobs"
-#print(msg)
-
-#printing my name via concatenation
-first_name = "Andrew"
-last_name = "Jacobs"
-full_name = first_name +" "+ last_name
-print(full_name)
